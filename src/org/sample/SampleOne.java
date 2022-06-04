8883fe7b899db62cf112011b97685f840dea4470
avkkumar1998 <avkkumar1998@gmail.com>
2022-06-04 20:24:31 +0530

    done

---
+++ src/org/sample/SampleOne.java
@@ -0,0 +1,17 @@
+package org.sample;
+
+public class SampleOne {
+	
+	private void sysyt() {
+		System.out.println("good");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		SampleOne s=new SampleOne();
+		
+		s.sysyt();
+	}
+
+}
