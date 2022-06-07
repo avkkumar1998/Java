48e940a28ffb4aad47df7ef29b96f2a262cef2dd
avkkumar1998 <avkkumar1998@gmail.com>
2022-06-07 19:17:29 +0530

    krishna codimg added

--- src/org/sample/SampleOne.java
+++ src/org/sample/SampleOne.java
@@ -6,12 +6,17 @@ public class SampleOne {
 		System.out.println("good");
 
 	}
+	private void tc2() {
+		System.out.println("bad");
+
+	}
 	
 	public static void main(String[] args) {
 		
 		SampleOne s=new SampleOne();
 		
 		s.sysyt();
+		s.tc2();
 	}
 
 }
