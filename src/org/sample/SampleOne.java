1908cf88dd8bc85976232adedfc6f0eb181a975e
avkkumar1998 <avkkumar1998@gmail.com>
2022-06-08 12:30:44 +0530

    Done sethu

--- src/org/sample/SampleOne.java
+++ src/org/sample/SampleOne.java
@@ -6,12 +6,17 @@ public class SampleOne {
 		System.out.println("good");
 
 	}
+	private void tc3() {
+		System.out.println("Normalize");
+
+	}
 	
 	public static void main(String[] args) {
 		
 		SampleOne s=new SampleOne();
 		
 		s.sysyt();
+		s.tc3();
 	}
 
 }
